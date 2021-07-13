7fd7e26efa6b0936a91a0d65049fea782c61394b
Flynn <wphestiraid@gmail.com>
2021-07-13 10:39:15 +0900

    Class-based Views 3 Using generic class-based views 학습

--- snippets/views.py
+++ snippets/views.py
@@ -9,28 +9,11 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, format=None):
-        return self.list(request, format)
 
-    def post(self, request, format=None):
-        return self.create(request, format)
-
-
-class SnippetDetail(
-    mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView
-):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
