6fb330e98a44840d1e1cee349694416cb02c7e66
Flynn <wphestiraid@gmail.com>
2021-07-13 10:08:58 +0900

    Serialization 학습

--- toy_django/urls.py
+++ toy_django/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+
 from toy_django.quickstart import views
 
 router = routers.DefaultRouter()
@@ -25,6 +26,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('snippets', include('snippets.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
