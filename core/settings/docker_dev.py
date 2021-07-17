37db9a3155dce123dc14cd98874841d0fcfb9e50
Flynn <wphestiraid@gmail.com>
2021-07-17 15:48:02 +0900

    django-redis 패키지 추가 및 설정 추가

--- core/settings/docker_dev.py
+++ core/settings/docker_dev.py
@@ -12,3 +12,11 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://cache:6379/1',
+        'OPTIONS': {'CLIENT_CLASS': 'django_redis.client.DefaultClient', 'PASSWORD': 'toypassword'},
+    }
+}
